a7bfcc54b5047d800867c539285b3d325904c8f9
Bessouat40 <71939720+Bessouat40@users.noreply.github.com>
2022-11-22 21:49:25 +0100

    Add files via upload

--- back/predict.py
+++ back/predict.py
@@ -1,5 +1,4 @@
 import joblib
-import cv2
 import numpy as np
 from PIL import Image
 from io import BytesIO
@@ -13,12 +12,11 @@ class Inference():
 
     def load_single_img(self, bytes) :
         image = BytesIO(bytes)
-        pil_image = Image.open(image).convert('RGB') 
+        pil_image = Image.open(image).convert('L') 
         img = np.array(pil_image)
-        img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
         img_shape = (150,150,1)
         img_width, img_height, nb_canaux = img_shape[0], img_shape[1], img_shape[2]
-        img = np.array([cv2.resize(img,(img_width, img_height))])
+        img = np.resize(img,(img_width, img_height))
         x = np.asarray([img])
         x = np.array(np.concatenate(x))
         x = x.reshape((1,150*150))
