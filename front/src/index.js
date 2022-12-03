ac860a43975e01247d373eb87059254b1e92b077
Bessouat40 <roman.bessouat@orange.fr>
2022-12-03 18:38:55 +0100

    add gitignore

--- front/src/index.js
+++ front/src/index.js
@@ -6,12 +6,27 @@ import {Grid } from "@mui/material";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <React.StrictMode >
+    <div 
+    // style={{
+    //     backgroundColor: '#200904',
+    //     margin: 0,
+    //     padding: 0,
+    //     position: "absolute",
+    //     top: 0,
+    //     right: 0,
+    //     bottom: 0,
+    //     left: 0,
+    //     margin: 0,
+    //     padding: 0
+    //   }}
+      >
     <Grid
-      spacing={3}
+      spacing={3} 
     >
       <FunctionalityChoice />
     </Grid>
+    </div>
   </React.StrictMode>
 );
 
