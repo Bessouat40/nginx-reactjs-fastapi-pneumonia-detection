e4989eff2419185af68ab71ae6c3c31f865a466c
Bessouat40 <roman.bessouat@orange.fr>
2022-12-15 20:28:27 +0100

    add nginx use

--- front/src/components/dropzone.js
+++ front/src/components/dropzone.js
@@ -1,4 +1,4 @@
-import React, {useCallback} from 'react'
+import React from 'react'
 import {useDropzone} from 'react-dropzone'
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import { Stack } from '@mui/system';
@@ -10,14 +10,14 @@ const Dropzone = ({onDrop}) => {
 
 
 return (
-    <div {...getRootProps()} style={{  alignItems: 'center',
+    <div {...getRootProps()} style={{  align: 'center',
     borderWidth: 2,
     borderRadius: 2,
     borderColor: '#eeeeee',
     borderStyle: 'dashed',
     backgroundColor: '#514d4c', width:300, height:100}}>
     <Stack alignItems='center'>
-        <CloudUploadIcon style={{width:50, height:50 }} alignItems='center'/>
+        <CloudUploadIcon style={{width:50, height:50 }} align='center'/>
     <input {...getInputProps()} />
         <Typography color="white">Upload your image(s)</Typography>
         </Stack>
