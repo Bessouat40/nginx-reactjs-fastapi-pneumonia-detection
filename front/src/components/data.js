47892de6545aa69b8609a2677b1504e78aeb8df5
Roman Bessouat <labess40@macbook-air-de-roman.home>
2022-12-28 09:46:26 +0100

    add database

---
+++ front/src/components/data.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Stack } from "@mui/system";
+import { Typography } from "@mui/material";
+
+const Data = () => {
+    return (
+        <Stack>
+            <Typography>Data here</Typography>
+        </Stack>
+    )
+}
+
+export default Data;
