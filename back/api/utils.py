f95d165aba2ded97c9ee5a14277b3c276868cab9
Roman Bessouat <labess40@MacBook-Air-de-Roman.local>
2023-01-17 01:03:25 +0100

    add utils documentation

--- back/api/utils.py
+++ back/api/utils.py
@@ -1,10 +1,26 @@
 import re
 
 def recognize_data(datas) :
+    """Recognize data from Form Data send by frontend
+
+    Args:
+        datas (str): string corresponding to bytes decoding
+
+    Returns:
+        match: regex matches from datas
+    """
     match = re.findall(r'\[([^\]]*)\]*', datas)
     return match
 
 def split_data(datas) :
+    """Split data before storing them into Postgres database
+
+    Args:
+        datas (List[str]): List containing regex matches
+
+    Returns:
+        final_data: data to store into Postgres database
+    """
     final_data = []
     for data in datas :
         split_data = data.replace('"', '').split(',')
